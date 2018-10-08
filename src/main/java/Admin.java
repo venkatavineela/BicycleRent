fae3bc5f1e1176763a2077c0ac273aec411ac64d
Vineela Vineela <vineela.vini421.vv@gmail.com>
2018-10-08 17:20:05 +0530

    Add the customerList in Admin class

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Admin {
 
+    private List<Customer> customerList = new ArrayList<>();
+
     void addCustomerRecordToInvoice(Customer customer, CustomerRecord record) {
+        if (!customerList.contains(customer)) {
+            customerList.add(customer);
+        }
         customer.invoice.add(record);
     }
 
